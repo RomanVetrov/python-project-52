663a5d695af50201fa04f5291642e5ea4371dc3a
RomanVetrov <romanvikernes@gmail.com>
2026-01-21 21:06:46 +0300

    Добавлены CRUD статусов и страница списка

--- statuses/views.py
+++ statuses/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models.deletion import ProtectedError
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
-# Create your views here.
+from .forms import StatusForm
+from .models import Status
+
+
+class StatusListView(LoginRequiredMixin, ListView):
+    model = Status
+    template_name = 'statuses/list.html'
+    context_object_name = 'statuses'
+
+
+class StatusCreateView(LoginRequiredMixin, CreateView):
+    model = Status
+    form_class = StatusForm
+    template_name = 'statuses/create.html'
+    success_url = reverse_lazy('statuses:list')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Статус успешно создан')
+        return super().form_valid(form)
+
+
+class StatusUpdateView(LoginRequiredMixin, UpdateView):
+    model = Status
+    form_class = StatusForm
+    template_name = 'statuses/update.html'
+    success_url = reverse_lazy('statuses:list')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Статус успешно изменён')
+        return super().form_valid(form)
+
+
+class StatusDeleteView(LoginRequiredMixin, DeleteView):
+    model = Status
+    template_name = 'statuses/delete.html'
+    success_url = reverse_lazy('statuses:list')
+
+    def form_valid(self, form):
+        try:
+            response = super().form_valid(form)
+        except ProtectedError:
+            messages.error(self.request, 'Невозможно удалить статус, потому что он используется')
+            return self.render_to_response(self.get_context_data())
+
+        messages.success(self.request, 'Статус успешно удалён')
+        return response
+
