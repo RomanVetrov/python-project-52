6bfc553dc335c029cb17ffad7f64b323bd331ef0
RomanVetrov <romanvikernes@gmail.com>
2026-01-26 17:11:19 +0300

    Покрыть фильтрацию задач тестами

--- tasks/tests.py
+++ tasks/tests.py
@@ -14,7 +14,9 @@ class TasksCrudTests(TestCase):
         self.user1 = User.objects.get(username="user1")
         self.user2 = User.objects.get(username="user2")
         self.status = Status.objects.create(name="новый")
+        self.status2 = Status.objects.create(name="в работе")
         self.label = Label.objects.create(name="bug")
+        self.label2 = Label.objects.create(name="feature")
         self.task = Task.objects.create(
             name="Task 1",
             description="Desc",
@@ -23,6 +25,14 @@ class TasksCrudTests(TestCase):
             executor=self.user2,
         )
         self.task.labels.add(self.label)
+        self.other_task = Task.objects.create(
+            name="Task 2",
+            description="Another",
+            status=self.status2,
+            author=self.user2,
+            executor=self.user1,
+        )
+        self.other_task.labels.add(self.label2)
 
     def test_guest_redirects_with_message(self):
         response = self.client.get(reverse("tasks:list"), follow=True)
@@ -67,6 +77,36 @@ class TasksCrudTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Task 1")
         self.assertContains(response, "bug")
+
+    def test_filter_by_status(self):
+        self.client.login(username="user1", password="pass12345")
+        response = self.client.get(
+            reverse("tasks:list"), {"status": self.status2.id}
+        )
+        tasks = response.context["tasks"]
+        self.assertEqual(list(tasks), [self.other_task])
+
+    def test_filter_by_executor(self):
+        self.client.login(username="user1", password="pass12345")
+        response = self.client.get(
+            reverse("tasks:list"), {"executor": self.user2.id}
+        )
+        tasks = response.context["tasks"]
+        self.assertIn(self.task, tasks)
+        self.assertNotIn(self.other_task, tasks)
+
+    def test_filter_by_label(self):
+        self.client.login(username="user1", password="pass12345")
+        response = self.client.get(reverse("tasks:list"), {"labels": self.label2.id})
+        tasks = response.context["tasks"]
+        self.assertEqual(list(tasks), [self.other_task])
+
+    def test_filter_by_author_self(self):
+        self.client.login(username="user1", password="pass12345")
+        response = self.client.get(reverse("tasks:list"), {"self_tasks": "on"})
+        tasks = response.context["tasks"]
+        self.assertIn(self.task, tasks)
+        self.assertNotIn(self.other_task, tasks)
 
     def test_only_author_can_delete_task(self):
         self.client.force_login(self.user2)  # <-- вместо login по паролю
